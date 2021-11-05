d99574cc6be8f01bb06e5492565828a9de653998
Junghun-Byun <79842508+Junghun-Byun@users.noreply.github.com>
2021-11-05 17:34:12 +0900

    Create 10718.cs

---
+++ 문제/단계별로 풀어보기/입출력과 사칙연산/2 We love kriii/10718.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(int i = 0; i < 2; i++) Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
