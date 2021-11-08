794410e5672c40b8d0d5db648a8835123bfece04
Junghun-Byun <79842508+Junghun-Byun@users.noreply.github.com>
2021-11-08 17:29:56 +0900

    Create 1001.cs

---
+++ 문제/단계별로 풀어보기/입출력과 사칙연산/6 A-B/1001.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
+
+            Console.WriteLine(input.Aggregate((A, B) => A - B));
+        }
+    }
+}
